25e22b51ff503edc446f7f386bc755715e839e9a
ZawMT <mg.zawmintun@gmail.com>
2021-02-06 05:39:36 +0630

    Trying gcc

---
+++ C/hello.c
@@ -0,0 +1,16 @@
+//Just trying gcc in macbook
+
+//How to compile this
+//gcc -o hello hello.c
+//This command will generate a file hello
+
+//How to run the result => ./hello
+//If there is any permission issue, we can make the file runnable by this command => chmod a+x hello
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello");
+	return 0;
+}
